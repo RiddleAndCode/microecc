ea6306609d0e364cb49cb0add6c043fc3fdd36d3
diogo <diogo@riddleandcode.com>
2018-07-10 17:55:49 +0200

    coding guideline jenkins

--- src/uECC.c
+++ src/uECC.c
@@ -209,7 +209,8 @@ int uECC_curve_public_key_size(uECC_Curve curve) {
 #if !asm_clear
 uECC_VLI_API void uECC_vli_clear(uECC_word_t *vli, wordcount_t num_words) {
   wordcount_t i;
-  for (i = 0; i < num_words; ++i) {
+  for (i = 0; i < num_words; ++i) 
+  {
     vli[i] = 0;
   }
 }
