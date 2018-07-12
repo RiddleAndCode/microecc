49db3e4544dfb893a5cb701ce960e656062a81fd
diogo <diogo@riddleandcode.com>
2018-07-12 13:50:59 +0200

    conan

---
+++ conanfile.py
@@ -0,0 +1,30 @@
+from conans import ConanFile, CMake, tools
+
+
+class MicroeccConan(ConanFile):
+    name = "microecc"
+    version = "1.0"
+    license = "<Put the package license here>"
+    url = "https://github.com/RiddleAndCode/microecc"
+    description = "Check git."
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=False"
+    generators = "cmake"
+    exports_sources = "src/*", "include/*", "*"
+
+    def build(self):
+        self.run('cmake .')
+        self.run('cmake --build .')
+
+    def package(self):
+        self.copy("*.h", dst="include", src="include")
+        self.copy("*.lib", dst="lib", keep_path=False)
+        self.copy("*.dll", dst="bin", keep_path=False)
+        self.copy("*.so", dst="lib", keep_path=False)
+        self.copy("*.dylib", dst="lib", keep_path=False)
+        self.copy("*.a", dst="lib", keep_path=False)
+
+    def package_info(self):
+        self.cpp_info.libs = ["microecc"]
+
